e9a13f67bde3daeaa94b17830b74d470955a9f84
Leo <leonardo.r.s.duarte@gmail.com>
2024-03-01 15:19:54 -0300

    Deploy do Projeto Projeto Integrador

--- src/components/postagens/cardPostagem/CardPostagem.tsx
+++ src/components/postagens/cardPostagem/CardPostagem.tsx
@@ -21,7 +21,7 @@ function CardPostagem({post}: CardPostagemProps) {
           <p>Data: {new Intl.DateTimeFormat(undefined, {
                     dateStyle: 'full',
                     timeStyle: 'medium',
-                  }).format(new Date(post.date))}</p>
+                  }).format(new Date(post.data))}</p>
         </div>
       </div>
       <div className="flex">
